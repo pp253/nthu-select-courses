806e50c77caef0f0027d0680dcaae26c6ef38bf9
pp253 <pp253@users.noreply.github.com>
2018-01-24 01:14:04 +0800

    change domain name

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -31,7 +31,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     hot: true,
     contentBase: false, // since we use CopyWebpackPlugin.
     compress: true,
-    host: 'pp253253.ddns.net', // HOST || config.dev.host,
+    host: 'nthu-course.ddns.net', // HOST || config.dev.host,
     port: PORT || config.dev.port,
     open: config.dev.autoOpenBrowser,
     overlay: config.dev.errorOverlay
