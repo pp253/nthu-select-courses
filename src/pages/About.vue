771455b5ddf9d3cc862c8acc902111436acc0e06
pp253 <pp253@users.noreply.github.com>
2018-06-10 02:05:09 +0800

    lint and update

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -29,42 +29,20 @@
             <v-container :pa-0="$store.state.ui.isMobile" pt-3 mb-3>
               <v-card>
                 <v-card-text>
-                  <v-layout
-                    v-for="(category, categoryName) in list"
-                    :key="categoryName"
-                    wrap
-                    pb-5
-                    justify-center
-                  >
-                    <v-flex xs12 pb-3><h2>{{ categoryName }}</h2></v-flex>
-                    <template
-                      v-for="people in category"
-                    >
-                      <v-flex
-                        v-if="people.text"
-                        :key="people.text"
-                        xs12
-                      >
+                  <v-layout v-for="(category, categoryName) in list" :key="categoryName" wrap pb-5 justify-center>
+                    <v-flex xs12 pb-3>
+                      <h2>{{ categoryName }}</h2>
+                    </v-flex>
+                    <template v-for="people in category">
+                      <v-flex v-if="people.text" :key="people.text" xs12>
                         <p v-html="people.text"></p>
                       </v-flex>
-                      <v-flex
-                        v-else
-                        :key="people.name"
-                        xs6 sm3 md3 lg2
-                      >
+                      <v-flex v-else :key="people.name" xs6 sm3 md3 lg2>
                         <v-container>
                           <a :href="people.link">
-                            <v-avatar
-                              size="64px"
-                              class="grey lighten-4"
-                            >
-                              <img
-                                v-if="people.img"
-                                :src="people.img"
-                              />
-                              <v-icon
-                                v-else
-                              >person</v-icon>
+                            <v-avatar size="64px" class="grey lighten-4">
+                              <img v-if="people.img" :src="people.img" />
+                              <v-icon v-else>person</v-icon>
                             </v-avatar>
                             <p class="mb-2">{{ people.name }}</p>
                           </a>
@@ -86,31 +64,20 @@
                 <v-card-text>
                   <h2 class="pb-3">依賴組件</h2>
                   <v-layout wrap justify-center>
-                    <v-flex
-                      v-for="dependency in dependencies"
-                      :key="dependency.name"
-                      xs4 sm3 md2 lg1
-                    >
+                    <v-flex v-for="dependency in dependencies" :key="dependency.name" xs4 sm3 md2 lg1>
                       <v-container>
                         <a :href="dependency.link">
-                          <v-avatar
-                            size="64px"
-                            class="grey lighten-4"
-                          >
-                            <img
-                              v-if="dependency.img"
-                              :src="dependency.img"
-                            />
-                            <v-icon
-                              v-else
-                            >extension</v-icon>
+                          <v-avatar size="64px" class="grey lighten-4">
+                            <img v-if="dependency.img" :src="dependency.img" />
+                            <v-icon v-else>extension</v-icon>
                           </v-avatar>
                           <p class="mb-2">{{ dependency.name }}</p>
                         </a>
                       </v-container>
                     </v-flex>
                   </v-layout>
-                  <p>更多請參考<a href="https://github.com/pp253/nthu-select-courses/blob/master/package.json">package.json</a>。</p>
+                  <p>更多請參考
+                    <a href="https://github.com/pp253/nthu-select-courses/blob/master/package.json">package.json</a>。</p>
                   <v-btn icon flat large @click="$router.push('/')">
                     <v-icon>home</v-icon>
                   </v-btn>
@@ -123,7 +90,9 @@
               <v-card>
                 <v-card-text>
                   <h2 class="pb-3">條款</h2>
-                  <p><a href="http://www.apache.org/licenses/">Apache-2.0</a></p>
+                  <p>
+                    <a href="http://www.apache.org/licenses/">Apache-2.0</a>
+                  </p>
                   <v-btn icon flat large @click="$router.push('/')">
                     <v-icon>home</v-icon>
                   </v-btn>
@@ -158,7 +127,7 @@ export default {
           },
           {
             text:
-              '<b>如何成為開發者？</b>請前往<a href="https://github.com/pp253/nthu-select-courses">GitHub</a>，然後commit個、或是PR，你就成為了開發者！'
+              '<b>如何成為開發者？</b>請前往<a href="https://github.com/pp253/nthu-select-courses">GitHub</a>，然後PR個，你就成為了開發者！'
           }
         ],
         測試者: [
